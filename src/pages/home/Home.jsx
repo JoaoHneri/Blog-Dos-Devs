e0f74ffa20c106ceae3ebd0417a4d4a92e350f4c
João Henrique <joaohnsousa@gmail.com>
2024-01-19 20:53:48 -0300

    Puxando as Publicações na Home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,44 @@
-
 import "./Home.css";
 import { useNavigate, Link } from "react-router-dom";
 import { useState, useEffect } from "react";
-const Home = () => {
-  return (
-    <div>
-      <h1>Olá</h1>
-    </div>
-  )
-}
+import { useFetchDocuments } from "../../hooks/useFetchDocument.";
 
-export default Home
+const Home = () => {
+  const [search, setSearch] = useState("");
+  const {documents: posts, loading} = useFetchDocuments("posts");
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  };
+
+  return (
+    <div className="home">
+      <h1>Veja os posts mais recentes dos devs de todo o Brasil</h1>
+      <form onSubmit={handleSubmit} className="search_form">
+        <input
+          type="text"
+          placeholder="Ou busque por tags"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button className="btn btn-dark">Pesquisar</button>
+      </form>
+      <div>
+        {loading && <p>Carregando...</p>}
+        {posts && posts.map((post) => (
+          <h3>{post.title}</h3>
+        ))}
+        {posts && posts.length === 0 && (
+          <div className="noposts">
+            {" "}
+            <p>Não foram encontrados posts</p>{" "}
+            <Link to="/posts/create" className="btn">
+              Criar primeiro post
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
